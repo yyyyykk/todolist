2ecae797ecae171a061e777b96e38cbc9e9e9344
hinafuyu <hirata90730@outlook.jp>
2023-04-14 15:00:25 +0900

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/folders/{id}/task', [TaskController::class, 'index'])->name('tasks.index');
